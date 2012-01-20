44b9f059786bbb589b8750200b544a42c963cba6
Albert Vilella <avilella@gmail.com>
2012-01-20 09:25:24 +0000

    adding paired for sga preprocess, indexfilteronly option

--- scripts/sga_cluster.pl
+++ scripts/sga_cluster.pl
@@ -3,7 +3,7 @@ use Getopt::Long;
 use strict;
 use File::Basename;
 
-my ($readsfile,$phred64,$no_permute,$dust,$sample,$readsfile,$onlyindex,$minreadlen,$tag,$exhaustive_overlap,$overlap,$csize,$threads,$debug);
+my ($readsfile,$phred64,$no_permute,$dust,$sample,$readsfile,$indexfilteronly,$onlyindex,$minreadlen,$tag,$exhaustive_overlap,$overlap,$csize,$threads,$debug,$paired);
 my $disk = 1000000;
 $threads = 1;
 $overlap = 31;
@@ -25,14 +25,17 @@ GetOptions(
 	   'disk:s' => \$disk,
 	   'sample:s' => \$sample,
 	   'phred64' => \$phred64,
+	   'paired' => \$paired,
 	   'no_permute' => \$no_permute,
            'd|debug:s' => \$debug,
            'onlyindex:s' => \$onlyindex,
+           'indexfilteronly:s' => \$indexfilteronly,
 	   'sga_exe:s' => \$sga_executable,
           );
 
+my ($infilebase,$path,$type) = fileparse($readsfile);
 if (!defined $tag) {
-  $tag = $readsfile . ".clst";
+  $tag = $infilebase . ".clst";
 };
 my $exhaustive = '';
 $exhaustive = '--exhaustive' if (1 == $exhaustive_overlap);
@@ -45,15 +48,18 @@ my $cmd;
 my $dust_threshold = '';
 my $sample_threshold = '';
 my $phred64_flag = '';
+my $paired_flag = '';
 my $permute_ambiguous = '';
 $dust_threshold = "--dust-threshold=" . $dust if (length $dust > 0);
 $sample_threshold = "--sample=" . $sample if (length $sample > 0);
 $phred64_flag = "--phred64" if ($phred64);
 $permute_ambiguous = "--permute-ambiguous" unless ($no_permute);
+if ($paired) { $readsfile =~ s/\:/\ /; $paired_flag = '--pe-mode=1'};
 # sga preprocess
-$cmd = "$sga_executable preprocess $sample_threshold $phred64_flag --min-length=$minreadlen $dust_threshold $permute_ambiguous $readsfile -o $tag.fq 2>$tag.preprocess.txt";
+$cmd = "$sga_executable preprocess $sample_threshold $paired_flag $phred64_flag --min-length=$minreadlen $dust_threshold $permute_ambiguous $readsfile -o $tag.fq 2>$tag.preprocess.txt";
 print STDERR "$cmd\n" if ($debug);
 $rerun_string .= "$cmd\n";
+$DB::single=1;1;
 unless(system("$cmd") == 0) {    print("$cmd\n");    throw("error running sga preprocess: $!\n");  }
 
 # sga index
@@ -63,16 +69,19 @@ print STDERR "$cmd\n" if ($debug);
 $rerun_string .= "$cmd\n";
 unless(system("$cmd") == 0) {    print("$cmd\n");    throw("error running sga index: $!\n");  }
 
-# sga rmdup
-$cmd = "$sga_executable rmdup -e $erate -t $threads $tag.fq";
-print STDERR "$cmd\n" if ($debug);
-$rerun_string .= "$cmd\n";
-unless(system("$cmd") == 0) {    print("$cmd\n");    throw("error running sga rmdup: $!\n");  }
-
 exit 0 if ($onlyindex);
 
+# sga rmdup
+#$cmd = "$sga_executable rmdup -e $erate -t $threads";
+$cmd = "$sga_executable filter --no-kmer-check -t $threads $tag.fq";
+print STDERR "$cmd\n" if ($debug);
+$rerun_string .= "$cmd\n";
+unless(system("$cmd") == 0) {    print("$cmd\n");    throw("error running sga filter: $!\n");  }
+
+exit 0 if ($indexfilteronly);
+
 # sga cluster
-$cmd = "$sga_executable cluster -m $overlap -c $csize -e $erate -t $threads $tag.rmdup.fa -o $tag.d$dust.$csize.$overlap.e$erate.clusters";
+$cmd = "$sga_executable cluster -m $overlap -c $csize -e $erate -t $threads $tag.filter.pass.fa -o $tag.d$dust.$csize.$overlap.e$erate.clusters";
 print STDERR "$cmd\n" if ($debug);
 $rerun_string .= "$cmd\n";
 unless(system("$cmd") == 0) {    print("$cmd\n");    throw("error running sga cluster: $!\n");  }
