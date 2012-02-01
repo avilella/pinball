7db23e2e012cfccebcbf3055a234a5201303365d
Albert Vilella <avilella@gmail.com>
2012-02-01 15:46:47 +0000

    adding format as option, fasta as default, would work for fastq as well

--- scripts/fasta2cluster.pl
+++ scripts/fasta2cluster.pl
@@ -3,18 +3,20 @@ use strict;
 use Getopt::Long;
 use Bio::SeqIO;
 
-my ($inputfile,$debug,$cluster_id,$limit);
+my ($inputfile,$debug,$cluster_id,$limit,$format);
+$format = 'fasta';
 $limit = 999999999999;
 GetOptions(
 	   'i|input|inputfile:s' => \$inputfile,
            'd|debug:s' => \$debug,
            'c|cluster_id:s' => \$cluster_id,
            'limit:s' => \$limit,
+           'format:s' => \$format,
           );
 
 my $io = Bio::SeqIO->new
 (-file => $inputfile,
- -format => 'fasta');
+ -format => $format);
 my $hash;
 my $count = 0;
 while (my $seq = $io->next_seq) {
@@ -22,6 +24,7 @@ while (my $seq = $io->next_seq) {
   my $display_id = $seq->display_id;
   my ($cluster,$cid,$walk,$wid) = split("-",$display_id);
   my $this_cluster_id;
+  $DB::single=1;1;
   if (defined($cluster_id)) {
     $this_cluster_id = $cluster_id;
   } else {
