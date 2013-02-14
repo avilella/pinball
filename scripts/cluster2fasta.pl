4074737fdaf605255dc1dbeeef565f018a2e33f2
Albert Vilella <avilella@gmail.com>
2013-02-14 10:25:50 +0000

    changed to minsize and maxsize

--- scripts/cluster2fasta.pl
+++ scripts/cluster2fasta.pl
@@ -3,10 +3,12 @@ use strict;
 use Getopt::Long;
 use Bio::SeqIO;
 
-my ($inputfile,$debug);
+my ($inputfile,$debug,$minsize,$maxsize);
 
 GetOptions(
 	   'i|input|inputfile:s' => \$inputfile,
+	   'min_csize:s' => \$minsize,
+	   'max_csize:s' => \$maxsize,
            'd|debug:s' => \$debug,
           );
 
@@ -14,6 +16,12 @@ open FILE,$inputfile or die $!;
 while (<FILE>) {
   chomp $_;
   my ($cluster_id,$num,$seq_id,$seq) = split(" ",$_);
+  if (defined $minsize) {
+    next if ($num < $minsize);
+  }
+  if (defined $maxsize) {
+    next if ($num > $maxsize);
+  }
   print "\>$seq_id.$cluster_id\n$seq\n";
 }
 
